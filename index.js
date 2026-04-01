6154a02e16c6d36beca1537083be8734aec5d53b
camilaokita <camila.okita@escola.pr.gov.br>
2026-04-01 09:05:04 -0300

    .

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
-console.log 
-
 const num1 = Number(prompt('Primeiro número:'));
 const num2 = Number(prompt('Segundo número:'));
 
 //soma
-soma = num1 + num2;
+let soma = num1 + num2;
 
 //subtração
-sub = num1 - num2;
+let sub = num1 - num2;
 
 //multiplicação
-mult = num1 * num2;
+let mult = num1 * num2;
 
 //divisão
-div = num1 / num2;
-
-const res1 = soma
-const res2 = sub
-const res3 = mult
-const res4 = div
+let div = num1 / num2;
